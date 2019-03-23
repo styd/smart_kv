483c9147cdfc9d767f6cecffc4c52b7e7cab1ce1
Adrian Setyadi <a.styd@yahoo.com>
2019-03-23 12:49:00 +0700

    Clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,38 +16,3 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
-
-# Swapping constants is for when I develop this gem along with other project that uses it.
-# I don't want constants to clash when I run the specs together.
-#
-# For example:
-#   bin/rspec spec/models/user_spec.rb ../smart_kv/spec/
-#
-# Maybe I'm overly cautious but better safe than sorry.
-#
-# def safely_swap_constant(original_constant_str)
-#   if (klass = Object.const_get(original_constant_str) rescue nil)
-#     Object.const_set("AVeryLongConstantToStore#{ original_constant_str }", klass)
-#     Object.send(:remove_const, original_constant_str.to_sym)
-#   end
-# end
-#
-# def safely_swap_back_constant(original_constant_str)
-#   Object.send(:remove_const, original_constant_str.to_sym)
-#   if (klass = Object.const_get("AVeryLongConstantToStore#{ original_constant_str }") rescue nil)
-#     Object.const_set(original_constant_str, klass)
-#     Object.send(:remove_const, "AVeryLongConstantToStore#{ original_constant_str }".to_sym)
-#   end
-# end
-#
-# def safely_swap_all_constants(array_of_constant_str)
-#   array_of_constant_str.each do |original_constant_str|
-#     safely_swap_constant(original_constant_str)
-#   end
-# end
-#
-# def safely_swap_back_all_constants(array_of_constant_str)
-#   array_of_constant_str.each do |original_constant_str|
-#     safely_swap_back_constant(original_constant_str)
-#   end
-# end
